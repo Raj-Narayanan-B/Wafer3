52e0f4c1bf204bca58fdbdff920fb07dddc46f0c
Raj Narayanan <brajnarayanan.b@gmail.com>
2023-09-15 12:11:04 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Check and replace the status of Semi-conductor Wafers using MLOPS',
+    author='Raj Narayanan',
+    license='MIT',
+)
